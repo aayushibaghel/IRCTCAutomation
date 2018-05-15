7cb35bd1b2f67b336bd7cd1ace02a88cc34e359c
Aayushi Baghel <aayushi.baghel@gmail.com>
2018-05-16 02:19:23 +0530

    created class for login

---
+++ src/com/selenium/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package com.selenium.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+
+public class LoginPage {
+	
+	WebDriver driver = null;
+	
+	public LoginPage(WebDriver driver)
+	{
+		this.driver= driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
